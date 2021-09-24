abd69f4f593d3328305e16b50469108b12f77b4c
aimanhungkul <muhammadanasaiman0@gmail.com207006101@student.unsil.ac.idgit initgit add .git commit -m first>
2021-09-24 14:42:59 +0800

    first commit

---
+++ pewarisan.php
@@ -0,0 +1,20 @@
+<? php
+class Karyawan extends Manusia{
+	var $nomor_pegawai;
+	var $jabatan;
+	var $departemen;
+function __construct() 
+function tampilnomor_pegawai ($nomorbaru){
+	$this -> nomor_pegawai = $nomorbaru
+}
+function tampiljabatan ($jabatanbaru){
+	$this -> jabatan = $jabatanbaru
+}
+function tampildepartemen ($departemenbaru){
+	$this -> departemen = $departemenbaru
+}
+}
+?>
+
+
+
